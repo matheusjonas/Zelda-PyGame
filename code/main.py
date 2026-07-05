274f8785db087d31a78270cefa93f9bfa5524618
matheusjonas <contatocomercialmatheusjonas@gmail.com>
2026-07-05 12:39:46 -0300

    Update main.py
    Added Fullscreen feature using Alt+Enter and Escape key toggle mainting previous logic and system behaviour.

--- code/main.py
+++ code/main.py
@@ -6,7 +6,7 @@ class Game:
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("Zelda")
-
+        self.isFullscreen = False
         self.SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
         self.CLOCK = pygame.time.Clock()
         self.LEVEL = Level()
@@ -20,15 +20,29 @@ class Game:
             self.LEVEL.run()
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    print("QUITED!")
-                    pygame.quit()
-                    sys.exit()
+                    self.quit_game()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_TAB:
                         self.LEVEL.toggleMenu()
-
+                    elif event.key == pygame.K_ESCAPE:
+                        self.quit_game()
+                    elif event.key == pygame.K_RETURN:
+                        mods = pygame.key.get_mods()
+                        if mods & pygame.KMOD_ALT:
+                            self.isFullscreen = not self.isFullscreen
+                            if self.isFullscreen:
+                                # Tuple (0,0) to target the user's native monitor resolution
+                                self.SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+                            else:
+                                # Switch back to Windowed mode
+                                self.SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
             pygame.display.update()
             self.CLOCK.tick(FPS)
+
+    def quit_game(self):
+        print("QUIT!")
+        pygame.quit()
+        sys.exit()
 
 
 if __name__ == "__main__":
